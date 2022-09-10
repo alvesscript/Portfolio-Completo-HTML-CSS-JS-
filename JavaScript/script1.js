842fecec017211f6bfc359aa778b9d7cd73ff444
Alves Script <100441454+alvesscript@users.noreply.github.com>
2022-09-10 02:46:56 -0300

    Validação do formulario

---
+++ JavaScript/script1.js
@@ -0,0 +1,17 @@
+let email = document.getElementById('email');
+let form = document.querySelector('form');
+let text;
+
+form.addEventListener('submit', (e) => {
+  if (email.value == '') {
+  }
+  console.log(email.value);
+  e.proventDefault();
+});
+
+const github = querySelector('github');
+function getApiGitHub {
+fetch()
+}
+
+getApiGitHub
